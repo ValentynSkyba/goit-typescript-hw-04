4f49f22250be46ede4d9a82de5eac86b1c2681b2
ValentynSkyba <153025143+ValentynSkyba@users.noreply.github.com>
2024-04-29 13:46:40 +0100

    Initial commit

---
+++ src/4.ts
@@ -0,0 +1,56 @@
+class Key {
+    constructor(private signature: number = Math.random()) {}
+
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key) {}
+
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean = false;
+    protected tenants: Person[] = [];
+  
+    constructor(private key: Key) {}
+
+    comeIn(person: Person): void {
+        if (this.door) {
+          this.tenants.push(person);
+          console.log('The door is open.');
+        } else {
+          console.log('The door is closed.');
+        }
+      }
+    
+      abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+          this.door = true;
+          console.log('The door is open.');
+        } else {
+          console.log('The door is closed.');
+        }
+      }
+}
+
+const key = new Key();
+
+const house = new MyHouse(key);
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+
+house.comeIn(person);
+
+
+export {};
